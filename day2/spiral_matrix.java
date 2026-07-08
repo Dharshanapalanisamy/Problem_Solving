fee4b9da5bebdd045168da1db2197bfdc1f3ef6c
Darshana P S <darshanapalanisamy08@gmail.com>
2026-07-08 13:53:39 +0530

    Updated Day 2 spiral matrix solution

--- day2/spiral_matrix.java
+++ day2/spiral_matrix.java
@@ -3,7 +3,7 @@ public class spiral_matrix {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=0;i<i!=n-1;i++){
+        for(int i=0;i<n;i++){
         }
     }
 }
